2e9745eae3ec684af3525882ae6ea24e039b5066
MirkanPolat <Mirkanpolat@outlook.de>
2025-05-22 22:29:52 +0200

    adding html, scss and import commonmodule

--- src/app/fruitlist/fruitlist.component.ts
+++ src/app/fruitlist/fruitlist.component.ts
@@ -1,14 +1,15 @@
 import { Component } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-fruitlist',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './fruitlist.component.html',
   styleUrl: './fruitlist.component.scss'
 })
 export class FruitlistComponent {
-    fruitlist = [
+
+  fruitlist = [
     {
       name: "Apfel",
       img:"apple.png",
