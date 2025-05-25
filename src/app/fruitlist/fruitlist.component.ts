70659b17dd5b1025496d3c0e3d51af1c9b43a2f3
MirkanPolat <Mirkanpolat@outlook.de>
2025-05-25 14:25:54 +0200

    adding input from the parent fruitlist to the child singlefruit for giving the index of the fruits

--- src/app/fruitlist/fruitlist.component.ts
+++ src/app/fruitlist/fruitlist.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SinglefruitComponent } from './singlefruit/singlefruit.component';
 @Component({
   selector: 'app-fruitlist',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, SinglefruitComponent],
   templateUrl: './fruitlist.component.html',
   styleUrl: './fruitlist.component.scss'
 })
